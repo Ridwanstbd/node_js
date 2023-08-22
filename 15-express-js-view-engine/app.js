7ce330f4bc1c1a25823fa222e34bc91b43c55d2f
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2023-08-22 23:49:02 +0700

    view engine sudah full

--- 15-express-js-view-engine/app.js
+++ 15-express-js-view-engine/app.js
@@ -1,20 +1,49 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 // gunakan view engine
 app.set('view engine','ejs')
+app.use(expressLayouts)
 
 app.get('/', (req, res) => {
     // res.sendFile('./index.html', { root: __dirname})
-    res.render('index')
+    const mahasiswa =[
+        {
+            nama: 'Ridwan Setio Budi',
+            email: 'ridwansetiobudi@hotmail.com'
+        },
+        {
+            nama: 'Anggar Prastiyo',
+            email: 'anggarprastiyo@hotmail.com'
+        },
+        {
+            nama: 'Rahmat Alamsyah',
+            email: 'rahmatalamsyah@hotmail.com'
+        },
+    ]
+    res.render('index', 
+    { 
+        nama : "Ridwan Setio Budi Gesss", 
+        title:"Homepage", mahasiswa ,
+        layout : 'layouts/main-layout'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', 
+    { 
+        layout : 'layouts/main-layout',
+        title : "Aboutpage"
+    })
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact')
+    res.render('contact', 
+    { 
+        layout : 'layouts/main-layout',
+        title : "Contactpage"
+    })
 })
 
 app.get('/products/:id', (req,res) => {
@@ -26,5 +55,5 @@ app.use('/', (req,res)=>{
     res.send('route blank')
 })
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
