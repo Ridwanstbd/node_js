777d864d567aee933c247efa468aa7c64c9c9f99
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2023-08-29 22:26:14 +0700

    crud sederhana

---
+++ 19-contact-app-3/app.js
@@ -0,0 +1,213 @@
+// ! deklarasi pemanggilan modul express yang telah diinstal sebelumnya dengan npm install express di terminal
+const express = require('express')
+// ! delklarasi pemanggilan modul ejs (embedded java script template layout)
+const expressLayouts = require('express-ejs-layouts')
+// ! deklarasi pemanggilan local modul contact dari folder utils atau termasuk utility 
+const { loadContact , findContact , addContact , cekDuplikat, deleteContact, updateContacts} = require('./utils/contacts')
+// ! variabel untuk memanggil fungsi express
+const app = express()
+const { body, validationResult , check } = require('express-validator');
+// ! memenggil beberapa package
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const flash = require('connect-flash')
+// ! deklarasi port
+const port = 3000
+//! deklarasi pegunaan view engine (mesin penampil ejs)
+app.set('view engine','ejs')
+//! pemakaian my third party middlewire berupa express layout
+app.use(expressLayouts)
+
+//! application build in middlewire untuk membuka file yang secara default disembunyikan oleh express ke dalam web
+app.use(express.static('public'))
+app.use(express.urlencoded({extended : true}))
+
+// ! konfigurasi flash
+app.use(cookieParser('secret'))
+app.use(
+    session({
+        cookie: {maxAge : 6000},
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+}))
+app.use(flash())
+
+// ! router berupa middlewire yang get maksudnya kita meraih kedalam protokol http setelah port
+// ! pada parameter yang pertama diberikanlah subdomain 
+// ! pada parameter kedua merupakan callback (fungsi didalam parameter) yang mengeluarkan fungsi lagi untuk merender web subdomain tersebut
+app.get('/', (req, res) => {
+    // res.sendFile('./index.html', { root: __dirname})
+    //  ? simulasi pemanggilan data dari array dan ini arraynya
+    const mahasiswa =[
+        {
+            nama: 'Ridwan Setio Budi',
+            email: 'ridwansetiobudi@hotmail.com'
+        },
+        {
+            nama: 'Anggar Prastiyo',
+            email: 'anggarprastiyo@hotmail.com'
+        },
+        {
+            nama: 'Rahmat Alamsyah',
+            email: 'rahmatalamsyah@hotmail.com'
+        },
+    ]
+    // ! render untuk merenderkan ke web
+    // ! parameter pertama untuk nama yang mengarah ke index .ejs sebagai view engine 
+    // ! parameter kedua merupakan objek yang akan di kelola didalam web
+    res.render('index', 
+    { 
+        // ? property untuk di olah di tampilan web
+        nama : "Ridwan Setio Budi Gesss", 
+        title:"Homepage",
+        // ! jika berupa array boleh juga ditulis nama arraynya saja 
+        mahasiswa ,
+        // ? property untuk memanggil main-layout dari folder layout
+        layout : 'layouts/main-layout'
+    })
+})
+// ! callback berparameter req maksudnya kita mengirim permintaan ke server
+// ! callback berparameter res maksudnya server mengirim respons ke kita
+app.get('/about', (req, res) => {
+    res.render('about', 
+    { 
+        // ? berisi objek untuk memunculkan layout web tersebut
+        layout : 'layouts/main-layout',
+        title : "Aboutpage"
+    })
+})
+
+app.get('/contact', (req, res) => {
+    // ! mengimport fungsi dari contact.js
+    const contacts = loadContact()
+    // ! menampailkan ke web
+    res.render('contact', 
+    { 
+        layout : 'layouts/main-layout',
+        title : "Contactpage",
+        // ! array yang di di dalan file contact.json
+        contacts,
+        msg : req.flash('msg')
+    })
+})
+// ! halaman tambah contact
+app.get('/contact/add',(req,res)=>{
+    res.render('add-contact',{
+        layout: 'layouts/main-layout',
+        title : "Tambah Contact"
+    })
+})
+// ! pemrosesanan inputan data dari form menggunakan method post
+app.post('/contact',[
+    body('nama').custom((value) => {
+        // ? cek duplikat dari value
+        const duplikat = cekDuplikat(value)
+        // ? jika ada duplikat maka berikan error
+        if(duplikat){
+            throw new Error('nama contact sudah digunakan !')
+        }
+        // ? jika tidak ada duplikat maka benar
+        return true
+    }),
+    check('email','Email tidak valid !').isEmail(),
+    check('nohp','Nomor HP tidak valid !').isMobilePhone('id-ID')
+],(req,res)=>{
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        // return res.status(400).json({errors : errors.array()})
+        res.render('add-contact',{
+            layout: 'layouts/main-layout',
+            title : "Tambah Contact",
+            errors : errors.array()
+        })
+    } else {
+        // ? fungsi berasal dari contacts.js
+        addContact(req.body)
+        // ? menampilkan pesan sukses
+        req.flash('msg','Data Contact Sudah Berhasil ditambahkan !')
+        // ? fungsi bawaan
+        res.redirect('/contact')
+    }
+})
+
+// ! halaman proses delete
+app.get('/contact/delete/:nama', (req,res) => {
+    // ? cari kontak dulu
+    const contact = findContact(req.params.nama)
+
+    // ? jika yang dimasukan bukan dari kontak
+    if (!contact) {
+        res.status(404)
+        res.send(`<h2>404</h2>`)
+    } else {
+        deleteContact(req.params.nama)
+        // ? menampilkan pesan sukses
+        req.flash('msg','Data Contact Sudah Berhasil dihapus !')
+        // ? fungsi bawaan
+        res.redirect('/contact')
+    }
+})
+// ! halaman ubah contact
+app.get('/contact/edit/:nama',(req,res)=>{
+    const contact = findContact(req.params.nama)
+    res.render('edit-contact',{
+        layout: 'layouts/main-layout',
+        title : "Tambah Contact",
+        contact
+    })
+})
+// ! proses ubah data
+app.post('/contact/update',[
+    body('nama').custom((value, {req}) => {
+        // ? cek duplikat dari value
+        const duplikat = cekDuplikat(value)
+        // ? jika ada duplikat maka berikan error
+        if(value !== req.body.oldName && duplikat){
+            throw new Error('nama contact sudah digunakan !')
+        }
+        // ? jika tidak ada duplikat maka benar
+        return true
+    }),
+    check('email','Email tidak valid !').isEmail(),
+    check('nohp','Nomor HP tidak valid !').isMobilePhone('id-ID')
+],(req,res)=>{
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        // return res.status(400).json({errors : errors.array()})
+        res.render('edit-contact',{
+            layout: 'layouts/main-layout',
+            title : "Tambah Contact",
+            errors : errors.array(),
+            contact : req.body
+        })
+    } else {
+        // ? fungsi berasal dari contacts.js
+        updateContacts(req.body)
+        // ? menampilkan pesan sukses
+        req.flash('msg','Data Contact Sudah Berhasil diubah !')
+        // ? fungsi bawaan
+        res.redirect('/contact')
+    }
+})
+// ! halman detail contact
+app.get('/contact/:nama', (req, res) => {
+    // ! kita meminta parameter id ke server dengan fungsi findcontact dari yang telah diekspor contacts.js 
+    const contact = findContact(req.params.nama)
+    res.render('detail', 
+    { 
+        layout : 'layouts/main-layout',
+        title : "Detail Contact",
+        contact
+    })
+})
+
+// ! penanganan ketika server gagal meminta atau permintaan di subdomain tidak valid dengan middlewire2 diatas
+app.use((req,res)=>{
+    res.status(404)
+    res.send('route blank')
+})
+// ! server menderngar dan mengabarkan di terminal bahwa server telah berjalan di port yang telah ditentukan tanpa error atu gagal
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
---
+++ 19-contact-app-3/utils/contacts.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+// membuat folder data jika belum ada
+const dirPath = './data'
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath)
+}
+// membuat file contacts json jika belum ada
+const dataPath = './data/contacts.json'
+if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath,'[]','utf-8')
+}
+// ambil semua data di contact.json
+const loadContact = () => {
+    const file =fs.readFileSync('data/contacts.json','utf-8')
+    const contacts = JSON.parse(file)
+    return contacts
+}
+// temukan data dari contact 
+const findContact = (nama) => {
+    const contacts = loadContact()
+    const contact = contacts.find((contact)=> contact.nama.toLowerCase() === nama.toLowerCase())
+    return contact
+}
+//  menuliskan / menimpa data file contacts.json denagan data contact yang baru 
+const saveContact = (contacts) =>{
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts))
+}
+
+// tambah contact baru 
+const addContact = (contact)=>{
+    const contacts = loadContact()
+    contacts.push(contact)
+    saveContact(contacts)
+}
+//  cek nama yang duplikat
+const cekDuplikat = (nama) => {
+    const contacts = loadContact()
+    return contacts.find((contact)=> contact.nama === nama)  
+}
+// hapus contact
+const deleteContact = (nama) =>{
+    const contacts = loadContact()
+    // buat array baru tanpa yang dipilih 
+    const filteredContacts = contacts.filter((contact)=>contact.nama !== nama)
+    // penggantian untuk array baru di json
+    saveContact(filteredContacts)
+
+}
+// mengubah contact
+const updateContacts = (contactBaru) =>{
+    const contacts = loadContact()
+    // hilangkan contact yang namanya sama dengan oldName
+    const filteredContacts = contacts.filter((contact)=> contact.nama !== contactBaru.oldName)
+    delete contactBaru.oldName
+    filteredContacts.push(contactBaru)
+    saveContact(filteredContacts)
+}
+module.exports = { loadContact , findContact ,addContact,cekDuplikat,deleteContact,updateContacts }
